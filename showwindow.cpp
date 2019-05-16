19a5034d699533b43842d6dd17c40d60e98d00c1
MRXY001 <wxy19980615@gmail.com>
2019-05-16 20:38:08 +0800

    添加结果窗口

--- showwindow.cpp
+++ showwindow.cpp
@@ -10,6 +10,7 @@ void ShowWindow::initView()
 {
     QHBoxLayout * main_layout = new QHBoxLayout;
     QVBoxLayout* vlayout = new QVBoxLayout;
+    week_combo = new QComboBox(this);
     time_list = new QListWidget(this);
     numb_edit = new QTextEdit(this);
     vlayout->addWidget(week_combo);
@@ -18,11 +19,33 @@ void ShowWindow::initView()
     main_layout->addWidget(numb_edit);
     this->setLayout(main_layout);
 
-    // 开始统计
-    analyze();
+    // 添加周数
+    QStringList sl;
+    for (int i = 1; i < 30; i++)
+    {
+        sl << QString::number(i);
+    }
+    week_combo->addItems(sl);
+
+    // 链接信号槽
+    connect(week_combo, SIGNAL(currentIndexChanged(int)), this, SLOT(slotWeekChanged(int)));
+
+    // 开始统计分离的课程
+    analyze(times, numbs, clss);
 }
 
-void ShowWindow::analyze()
+/**
+ * 根据所有的内容进行解析，拆分成多个数组
+ * @param times 课程时间数组：周一第1,2节{第1-8周};周四第6,7节{第2-8周|双周};周四第8节{第2-8周|双周}
+ * @param numbs 上课人数数组，为一堆整数
+ * @param clss  进行统计的课程数量，如果有多个时间段就算作多个课程（因为部分具体那个名字）
+ */
+void ShowWindow::analyze(QStringList times, QStringList numbs, QList<Cls> &clss)
+{
+
+}
+
+void ShowWindow::slotWeekChanged(int x)
 {
 
 }
--- showwindow.h
+++ showwindow.h
@@ -8,26 +8,39 @@
 #include <QListWidget>
 #include <QTextEdit>
 #include <QComboBox>
+#include <QList>
 
 class ShowWindow : public QDialog
 {
     Q_OBJECT
+
+struct Cls {
+    int start_week, end_week; // 第几周
+    int week;   // 周日~周六，0~6
+    int course; // 课程：第1~12节课
+    bool dual; // 双周
+    int member; // 人数
+};
+
 public:
     ShowWindow(QWidget* parent, QString c1, QString c2);
 
 private:
     void initView();
-    void analyze();
+    void analyze(QStringList times, QStringList numbs, QList<Cls> &clss);
 
 signals:
 
 public slots:
+    void slotWeekChanged(int x);
 
 private:
     QStringList times, numbs;
     QComboBox* week_combo;
     QListWidget* time_list;
     QTextEdit* numb_edit;
+
+    QList<Cls> clss;
 };
 
 #endif // SHOWWINDOW_H
